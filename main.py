e563944c805550c0477d92dddbbee51dbe6b2f35
Sergei <whiteigel@gmail.com>
2023-11-28 21:57:34 +0100

    Minor changes

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from braille_dict import *
 
+BRAILLE_DICT = BRAILLE_DICT
+
 input_string = input(str('Введите слово: '))
-def convert_to_braille(message, braille_dict):
+def convert_to_braille(message, dictionary):
     message = message.upper()
     braille_message = ''
     for char in message:
-        if char in braille_dict:
-            braille_message += braille_dict[char] + ' '
+        if char in dictionary:
+            braille_message += dictionary[char] + ' '
         else:
             return "Неподдерживаемый язык"
     return braille_message.strip()
 
-braille_message = convert_to_braille(input_string, braille_dict)
+braille_message = convert_to_braille(input_string, BRAILLE_DICT)
 print("Сообщение:", braille_message)
